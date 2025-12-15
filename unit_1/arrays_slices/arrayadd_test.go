ea6541afa4f466d0c533e59435621eda3d3affc4
Anurag Vishwakarma <anuragvishwakarmaalt@gmail.com>
2025-12-15 22:55:58 +0530

    array and slices

--- unit_1/arrays_slices/arrayadd_test.go
+++ unit_1/arrays_slices/arrayadd_test.go
@@ -1,6 +1,8 @@
 package arraysslices
 
 import (
+	"reflect"
+	"slices"
 	"testing"
 )
 
@@ -15,4 +17,34 @@ func TestArrayAdd(t *testing.T) {
 		}
 	})
 
+	t.Run("Addition of multiple slices", func(t *testing.T) {
+		got := sumAll([]int{1, 1}, []int{1, 1})
+		want := []int{2, 2}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+}
+
+func TestArrayTails(t *testing.T) {
+
+	checksum := func(got []int, want []int, t *testing.T) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("Addition of tails only", func(t *testing.T) {
+		got := sumAllTails([]int{0, 1}, []int{2, 3})
+		want := []int{1, 3}
+		checksum(got, want, t)
+	})
+
+	t.Run("Checking if empty slice is taken into consideration", func(t *testing.T) {
+		got := sumAllTails([]int{}, []int{1, 2})
+		want := []int{0, 2}
+		checksum(got, want, t)
+	})
 }
--- unit_1/arrays_slices/arrays.go
+++ unit_1/arrays_slices/arrays.go
@@ -11,3 +11,28 @@ func arrayAddition(numbers []int) int {
 	}
 	return sum
 }
+
+func sumAll(numbersSlices ...[]int) []int {
+
+	var finalSlice []int
+
+	for _, number := range numbersSlices {
+		finalSlice = append(finalSlice, arrayAddition(number))
+	}
+	return finalSlice
+
+}
+
+func sumAllTails(numberSlices ...[]int) []int {
+	var finalSlice []int
+
+	for _, number := range numberSlices {
+		if len(number) == 0 {
+			finalSlice = append(finalSlice, 0)
+			continue
+		}
+		tail := number[1:]
+		finalSlice = append(finalSlice, arrayAddition(tail))
+	}
+	return finalSlice
+}
