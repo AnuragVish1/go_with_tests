23de5c4273537a4efc371ea3b46fa3f77d1eae99
Anurag Vishwakarma <anuragvishwakarmaalt@gmail.com>
2025-12-31 15:59:31 +0530

    some refactoring

--- unit_1/reading/blogposts/blogposts.go
+++ unit_1/reading/blogposts/blogposts.go
@@ -1,5 +1,39 @@
 package blogposts
 
+import (
+	"errors"
+	"io/fs"
+)
+
 func main() {
 
+}
+
+func NewPostFromFS(filesystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(filesystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	posts := []Post{}
+
+	for _, file := range dir {
+		post, err := getPost(filesystem, file.Name())
+		if err != nil {
+			return nil, errors.New("This i cant do")
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, file string) (Post, error) {
+	postFile, err := fileSystem.Open(file)
+
+	if err != nil {
+		return Post{}, err
+	}
+
+	defer postFile.Close()
+
+	return makeNewPost(postFile)
 }
--- unit_1/reading/blogposts/post.go
+++ unit_1/reading/blogposts/post.go
@@ -3,10 +3,8 @@ package blogposts
 import (
 	"bufio"
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
-	"io/fs"
 	"strings"
 )
 
@@ -30,35 +28,6 @@ const (
 	tagsStart        = "Tags: "
 	bodyStart        = "---"
 )
-
-func NewPostFromFS(filesystem fs.FS) ([]Post, error) {
-	dir, err := fs.ReadDir(filesystem, ".")
-	if err != nil {
-		return nil, err
-	}
-	posts := []Post{}
-
-	for _, file := range dir {
-		post, err := getPost(filesystem, file.Name())
-		if err != nil {
-			return nil, errors.New("This i cant do")
-		}
-		posts = append(posts, post)
-	}
-	return posts, nil
-}
-
-func getPost(fileSystem fs.FS, file string) (Post, error) {
-	postFile, err := fileSystem.Open(file)
-
-	if err != nil {
-		return Post{}, err
-	}
-
-	defer postFile.Close()
-
-	return makeNewPost(postFile)
-}
 
 func makeNewPost(postFile io.Reader) (Post, error) {
 	scanner := bufio.NewScanner(postFile)
